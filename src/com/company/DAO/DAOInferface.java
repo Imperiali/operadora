5bd0b7c3ff1ab704300952e599911a278371b258
imperiali <igor.imperiali@gmail.com>
2019-06-09 17:12:55 -0300

    Interface para metodos de acesso aos dados

---
+++ src/com/company/DAO/DAOInferface.java
@@ -0,0 +1,10 @@
+package com.company.DAO;
+
+import com.company.Operadora.Cliente;
+
+public interface DAOInferface {
+    private void adicionar(Cliente cliente){};
+    private void remover(Cliente cliente){};
+    private void alterar(Cliente cliente){};
+    private void listar(){};
+}
