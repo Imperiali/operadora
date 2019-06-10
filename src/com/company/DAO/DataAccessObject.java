1f8d6d4f39d5ab30a716c10771db458101537a40
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-06-10 08:55:05 -0300

    Atualizando classe DAO

--- src/com/company/DAO/DataAccessObject.java
+++ src/com/company/DAO/DataAccessObject.java
@@ -1,20 +1,92 @@
 package com.company.DAO;
 
-public class DataAccessObject implements DAOInferface{
-    private String nome;
+import com.company.Operadora.Cliente;
 
-    public DataAccessObject(String nome) {
-        this.nome = nome;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class DataAccessObject extends Arquivo implements DAOInferface{
+    private ArrayList<Cliente> clientes;
+
+    public DataAccessObject(String nomeArq) {
+        super(nomeArq);
+        clientes = new ArrayList<>();
+        Scanner entrada = abreArquivo();
+        if (entrada != null){
+            leArquivo(entrada, clientes);
+            fechaArquivo(entrada);
+        }
+    }
+
+    @Override
+    public void adicionar(Cliente cliente) {
+        clientes.add(cliente);
+        salvar();
+    }
+
+    @Override
+    public boolean remover(int indice) {
+        if (clientes.isEmpty() || indice < 0) {
+            return false;
+        } else {
+            clientes.remove(indice);
+            salvar();
+            return true;
+        }
+    }
+
+    @Override
+    public void alterar(int indice, Cliente cliente) {
+        clientes.set(indice, cliente);
+        salvar();
+    }
+
+    @Override
+    public String listar() {
+        String msg = "";
+        msg += "------------------------------\n";
+
+        if (msg.isEmpty()) {
+            msg = "Sem Clientes";
+        }
+        for(Cliente cliente:this.clientes){
+            msg += cliente + "\n";
+        }
+
+        msg += "------------------------------";
+
+        return msg;
+    }
+
+    private void salvar(){
+        Formatter saida = abreArquivoGravar();
+        if (saida != null){
+            gravaArquivo(saida, clientes);
+            fechaArquivo(saida);
+        }
+    }
+
+    public int pesquisaCliente(int num){
+        int pos = -1;
+        for (int i = 0; i < clientes.size(); i++){
+            if(clientes.get(i).getNumero() == num){
+                pos = i;
+                break;
+            }
+        }
+        return pos;
     }
 
     //    region Getters and Setters
-    public String getNome() {
-        return nome;
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
     }
 
-//    endregion
+    //    endregion
 }
