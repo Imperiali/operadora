60598080b5673878e531691b9b17c1304f6da9fc
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-06-14 09:32:33 -0300

    Classe operadora que gerencia clientes e ligacoes

--- src/com/company/Operadora/Operadora.java
+++ src/com/company/Operadora/Operadora.java
@@ -1,4 +1,136 @@
 package com.company.Operadora;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Operadora {
+  ArrayList<Cliente> clientes;
+  ArrayList<Ligacao> ligacoes;
+
+  public Operadora(ArrayList<Cliente> clientes) {
+    this.clientes = clientes;
+  }
+
+  public Operadora(ArrayList<Cliente> clientes, ArrayList<Ligacao> ligacoes) {
+    this.clientes = clientes;
+    this.ligacoes = ligacoes;
+  }
+
+  public String listarLigacoes() {
+    String msg = "";
+
+    msg += "------------------------------\n";
+
+    for(Ligacao ligacao:ligacoes){
+        msg += ligacao + "\n";
+    }
+
+    if (msg.isEmpty()) {
+      msg = "Sem Clientes";
+    }
+
+    msg += "------------------------------";
+
+    return msg;
+  }
+
+  public String mostrarClietneTop() {
+    ArrayList<Cliente> clientes = ordenarPorCredito();
+    String msg = "";
+
+    msg += "------------------------------\n";
+
+    if (msg.isEmpty()) {
+      msg = "Sem Clientes";
+    }else {
+      msg += clientes.get(clientes.size() -1) + "\n";
+    }
+
+    msg += "------------------------------";
+
+    return msg;
+  }
+
+  public String listarPorCreditoAcimaDe(float credito) {
+    ArrayList<Cliente> clientes = ordenarPorCredito();
+    String msg = "";
+
+    msg += "------------------------------\n";
+
+    for(Cliente cliente:clientes){
+      if (cliente.getCreditos() >= credito){
+        msg += cliente + "\n";
+      }
+    }
+
+    if (msg.isEmpty()) {
+      msg = "Sem Clientes";
+    }
+
+    msg += "------------------------------";
+
+    return msg;
+  }
+
+  public String listarSemCredito() {
+    ArrayList<Cliente> clientes = ordenarPorCredito();
+    float abaixoDe = 0;
+    String msg = "";
+
+    msg += "------------------------------\n";
+
+    for(Cliente cliente:clientes){
+      if (cliente.getCreditos() <= abaixoDe){
+        msg += cliente + "\n";
+      }
+    }
+
+    if (msg.isEmpty()) {
+      msg = "Sem Clientes";
+    }
+
+    msg += "------------------------------";
+
+    return msg;
+  }
+
+  private ArrayList<Cliente> ordenarPorCredito(){
+    ArrayList<Cliente> clientesOrdenados = clientes;
+
+    Collections.sort(clientesOrdenados);
+
+    return clientesOrdenados;
+  }
+
+  public int pesquisaCliente(int num){
+    int pos = -1;
+    for (int i = 0; i < clientes.size(); i++){
+      if(clientes.get(i).getNumero() == num){
+        pos = i;
+        break;
+      }
+    }
+    return pos;
+  }
+
+  //region Getters and Setters
+
+  public ArrayList<Cliente> getClientes() {
+    return clientes;
+  }
+
+  public void setClientes(ArrayList<Cliente> clientes) {
+    this.clientes = clientes;
+  }
+
+  public ArrayList<Ligacao> getLigacoes() {
+    return ligacoes;
+  }
+
+  public void setLigacoes(ArrayList<Ligacao> ligacoes) {
+    this.ligacoes = ligacoes;
+  }
+
+  //endregion
+
 }
