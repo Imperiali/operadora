3aa8c86ce849d76ed6764338162b753953481095
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-06-14 09:33:54 -0300

    Atualizando inteface DAO

--- src/com/company/DAO/DAOInferface.java
+++ src/com/company/DAO/DAOInferface.java
@@ -2,9 +2,12 @@ package com.company.DAO;
 
 import com.company.Operadora.Cliente;
 
+import java.util.ArrayList;
+
 public interface DAOInferface {
     void adicionar(Cliente cliente);
     boolean remover(int num);
     void alterar(int indice, Cliente cliente);
-    String listar();
+    String mostrar();
+    ArrayList<Object> listarTodos();
 }
